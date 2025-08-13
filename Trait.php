0af0243edcb0dcc2ecf007f3498b6c130debcb53
John Doe <johndoe@example.com>
2025-08-13 16:04:48 +0700

    Trait

---
+++ Trait.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/data/SayGoodBye.php';
+
+use Data\Traits\{Person, SayGoodBye, SayHello};
+
+$person = new Person();
+$person->goodBye('Joko');
+$person->hello('Budi');
+
+$person->name = 'Mugiew';
+
+var_dump($person);
