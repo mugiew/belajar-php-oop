1ae1eb6f4b81405c978779ca2b29f5086a7b0017
John Doe <johndoe@example.com>
2025-08-13 13:25:12 +0700

    GetterAndSetter

---
+++ GetterAndSetter.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/data/Category.php';
+
+$category = new Category();
+$category->setName('Handphone');
+$category->setExpensive(true);
+
+echo 'Name: ' . $category->getName() . PHP_EOL;
+echo 'Expensive: ' . $category->isExpensive() . PHP_EOL;
